579827b60580b6951a2cee7063a99b047ec71284
GiilDe <38991777+gilad212387@users.noreply.github.com>
2019-09-08 21:39:55 +0300

    Added graph prints and trained model save

--- resnet_with_block.py
+++ resnet_with_block.py
@@ -3,6 +3,7 @@ from torchvision.models import ResNet
 from layer_blocks import SELayer, SRMWithCorrMatrix, SRMLayer, SRMWithMedian
 import layer_blocks
 
+
 def conv3x3(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
                      padding=1, bias=False)
